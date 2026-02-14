5ef927619042f735894d7d3ceb3f9563c437bdb7
Adarsh Tiwari <adarsh03082002@gmail.com>
2026-02-14 14:04:20 +0530

    Updated project files

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from './components/Services/Services.jsx';
 import Banner from './components/Banner/Banner.jsx';
 import Subscribe from './components/Subscribe/Subscribe.jsx';
 import Banner2 from './components/Banner/Banner2.jsx';
+import Footer from './components/Footer/Footer.jsx';
 const App = () => {
   return (
     <main className='overflow-x-hidden bg-white text-dark'>
@@ -14,6 +15,7 @@ const App = () => {
       <Banner/>
       <Subscribe/>
       <Banner2/>
+      <Footer/>
 
       </main>
     
